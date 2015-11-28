39040d92fdf502398a6f63ea70ed46f8d9a4e78a
kanin <kanin30406@gmail.com>
2015-11-28 14:40:20 +0700

    start

---
+++ app/src/main/java/wiboosirichai/kanin/mytraffic/MainActivity.java
@@ -0,0 +1,17 @@
+package wiboosirichai.kanin.mytraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView trafficsViewListView;
+    private Button aboutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
