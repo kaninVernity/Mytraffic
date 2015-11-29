4596561d9bd1b1edebb90ac7a560d18f852c275d
kanin <kanin30406@gmail.com>
2015-11-29 11:53:05 +0700

    Adapter

--- app/src/main/java/wiboosirichai/kanin/mytraffic/MainActivity.java
+++ app/src/main/java/wiboosirichai/kanin/mytraffic/MainActivity.java
@@ -1,5 +1,8 @@
 package wiboosirichai.kanin.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,9 +30,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
-
-
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(),R.raw.phonton1);
+                buttonPlayer.start();
+                //////web view
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/KK9juxl_1LI"));
+                startActivity(objIntent);
 
             } // Event
         });
